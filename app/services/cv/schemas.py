c228f140f5b626579908be6d401c3925789dc0de
Ilya Zhuravlev <ichirozcom@gmail.com>
2024-06-13 19:48:29 +0300

    fix(user): fix validation for nullable fields

--- app/services/cv/schemas.py
+++ app/services/cv/schemas.py
@@ -7,24 +7,24 @@ from app.services.applicant.schemas import ApplicantXP, ApplicantEducation
 
 
 class CV(BaseModel):
-    login: str  # slug
-    avatar: str  # avatar
-    phone: str  # Моб
-    email: str  # email
-    bio: Optional[str]  # О себе
     id: str  # id из базы
-    salary_from: int  # ЗП
-    currency: str  # валюта
-    is_relocation: bool  # готов к переезду
-    is_remote: bool  # готов к удаленке
-    search_status_id: str  # статус поиска
+    login: str  # slug
+    avatar: Optional[str]  # avatar
+    phone: Optional[str]  # Моб
+    email: Optional[str]  # email
+    bio: Optional[str]  # О себе
+    salary_from: Optional[int]  # ЗП
+    currency: Optional[str]  # валюта
+    is_relocation: Optional[bool]  # готов к переезду
+    is_remote: Optional[bool]  # готов к удаленке
+    search_status_id: Optional[str]  # статус поиска
     qualification_id: Optional[str]  # квалификация
     division_id: Optional[str]  # специализация
-    city_id: str  # город
-    skill_set: str  # навыки
-    experience: str  # опыт работы
-    age: int  # возраст
-    title: str  # Имя фамилия
+    city_id: Optional[str]  # город
+    skill_set: Optional[str]  # навыки
+    experience: Optional[str]  # опыт работы
+    age: Optional[int]  # возраст
+    title: Optional[str]  # Имя фамилия
     registered_at: Optional[datetime]  # дата регистрации
     last_visited: Optional[datetime]  # дата последнего визита
     xp: List[ApplicantXP]  # опыт работы
